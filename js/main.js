1002eb5901c5632f0e0b4d6b2828ccaf2bbf9b9c
Ilona Sellenberkova <ilona.sell@gmail.com>
2018-11-06 18:20:14 +0100

    api solved

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'pk.eyJ1IjoiaWxseXNoZWxseSIsImEiOiJjam8zNzNienYwNmo2M3BudHI4aHl0aHU3In0.-LrVbThUJxxtMLHMHAnqnQ',
+    mapboxToken: api_key,
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
